056596e1629a6eed335c059eb0a9aab0073089fa
Harshitha1412 <147846515+Harshitha1412@users.noreply.github.com>
2024-06-15 15:00:16 +0530

    Update views.py

--- Sellermodule/views.py
+++ Sellermodule/views.py
@@ -29,28 +29,4 @@ def profile(request):
     # Logic for the register page
     return render(request, 'profile.html')
 =======
-from django.contrib.auth import authenticate, login
-from django.shortcuts import render, redirect
-
-
-# Create your views here.
-def login_user(request):
-    if request.user.is_authenticated:
-        return redirect("homepage")
-    if request.method == "POST":
-        username = request.POST.get('nm')
-        password = request.POST.get('pwd')
-        user = authenticate(request,username = username,password = password )
-        if user is not None:
-            login(request,user)
-            return redirect('homepage')
-        else:
-            return redirect("login")
-
-    return render(request,'login.html')
-
-def logout(request):
-    logout("request")
-    return redirect('login')
->>>>>>> 0e6f707d86d6c97ff38dd31439e3c8f8e166464b
 
